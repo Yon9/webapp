f8eb20aa3778d7622d45f4d30c399dcd0bd2edec
Yon9 <yongjin6435@naver.com>
2022-01-10 17:06:33 +0900

    2022-01-10

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-import logo from './logo.svg';
-import me from './me.png';
+import logo from './images/logo.svg';
+import me from './images/me.png';
+import instagram from './images/Instagram.svg';
+import mobile from './images/mobile.svg';
+import email from './images/email.svg';
 import './App.css';
 
 function App() {
@@ -26,7 +29,15 @@ function App() {
             <p>순천향대학교 경영학과 학사 졸업</p>
             <p>웹디자인/웹퍼블리셔 UIUX 양성과정 수료</p>
           </div>
+          <div className="sns">
+            <ul>
+              <li><a href="#"><img src={ instagram } alt="인스타그램" /></a></li>
+              <li><a href="#"><img src={ mobile } alt="연락처" /></a></li>
+              <li><a href="#"><img src={ email } alt="이메일" /></a></li>
+            </ul>
+          </div>
         </div>
+        
         <div className="right">
           <div className="search">
             <div className="searchBox">
@@ -35,10 +46,40 @@ function App() {
             <div className="searchButton"><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></div>
           </div>
           <div className="contents">
-            <div className="contentBox"></div>
-            <div className="contentBox"></div>
-            <div className="contentBox"></div>
+
+            <div className="contentBox">
+              <a href="#"><div className="imgBox"></div></a>
+            </div>
+            <div className="contentBox">
+              <a href="#"><div className="imgBox"></div></a>
+            </div>
+            <div className="contentBox">
+              <a href="#"><div className="imgBox"></div></a>
+            </div>
+            <div className="contentBox">
+              <a href="#"><div className="imgBox"></div></a>
+            </div>
+            <div className="contentBox">
+              <a href="#"><div className="imgBox"></div></a>
+            </div>
+            <div className="contentBox">
+              <a href="#"><div className="imgBox"></div></a>
+            </div>
+            <div className="contentBox">
+              <a href="#"><div className="imgBox"></div></a>
+            </div>
+            <div className="contentBox">
+              <a href="#"><div className="imgBox"></div></a>
+            </div>
+            <div className="contentBox">
+              <a href="#"><div className="imgBox"></div></a>
+            </div>
+            
+            
           </div>
+          <footer>
+            <p>COPYRIGHT &copy; YONGJIN JU 2022. All Right Reserved.</p>
+          </footer>
         </div>
       </div>
     </div>
